025b263fa15df5833c0d0b5db469445d3f043507
alreva <alreva@gmail.com>
2014-06-09 23:38:11 +0300

    addede scrumboard changes

--- KanbanImprove/board-scrum.js
+++ KanbanImprove/board-scrum.js
@@ -3,18 +3,13 @@
 // @namespace  http://tfs2010.it.volvo.net/s
 // @version    0.1
 // @description  Does the usability improvements over standard SharePoint Kanban board
-// @match      http://tfs2010.it.volvo.net:8080/tfs/Global/SEGOT-eCom-VolvoPentaShop/PentaTeam/_backlogs/board
+// @match      http://tfs2010.it.volvo.net:8080/tfs/Global/SEGOT-eCom-VolvoPentaShop/PentaTeam/_boards
 // @copyright  2014+, Volvo IT
 // ==/UserScript==a
 
 (function () {
 
-    var customStyle =
-    ".board-tile.cr {background-color: #3276b1; border-color: #285e8e; color: white} " +
-    ".board-tile.at {color: gray} " +
-    ".board-tile.expediter {background-color: #ed9c28; border-color: #d58512; color: white} " +
-    ".board-tile.blocked {background-color: #d2322d; border-color: #ac2925; color: white} "
-    ;
+    var isFocused = true;
 
     function addGlobalStyle(css) {
         var head, style;
@@ -26,8 +21,34 @@
         head.appendChild(style);
     }
 
+    function pretendTilesAreLikeInKanbanBoard() {
+
+        if (!isFocused || $("#taskboard-table").length < 1) {
+            setTimeout(pretendTilesAreLikeInKanbanBoard, 1000);
+            return;
+        }
+
+        $("[id^='taskboard-table_p']")
+            .each(function () {
+                var $elm = $(this);
+                var itemId = $elm.attr("id").slice("taskboard-table_p".length);
+                console.log("Kanban improve (Scrum): item #" + itemId);
+                $elm.closest("tr")
+                	.addClass("board-tile")
+                	.attr("data-item-id", itemId);
+            });
+    }
+
     $(function () {
-        addGlobalStyle(customStyle);
+
+        pretendTilesAreLikeInKanbanBoard();
+
+        addGlobalStyle(
+            ".board-tile.cr {background-color: #3276b1; border-color: #285e8e; color: white} " +
+            ".board-tile.at {color: gray} " +
+            ".board-tile.expediter {background-color: #ed9c28; border-color: #d58512; color: white} " +
+            ".board-tile.blocked {background-color: #d2322d; border-color: #ac2925; color: white} "
+        );
     });
 
 })(jQuery);
--- KanbanImprove/board.js
+++ KanbanImprove/board.js
@@ -3,8 +3,9 @@
 // @namespace  http://tfs2010.it.volvo.net/
 // @version    0.1
 // @description  Does the usability improvements over standard SharePoint Kanban board
-// @match      http://tfs2010.it.volvo.net:8080/tfs/Global/SEGOT-eCom-VolvoPentaShop/PentaTeam/_backlogs/board
 // @match      http://tfs2010.it.volvo.net:8080/tfs/Global/SEGOT-eCom-VolvoPentaShop/PentaBusiness/_backlogs/board
+// @match      http://tfs2010.it.volvo.net:8080/tfs/Global/SEGOT-eCom-VolvoPentaShop/PentaTeam/_backlogs/board
+// @match      http://tfs2010.it.volvo.net:8080/tfs/Global/SEGOT-eCom-VolvoPentaShop/PentaTeam/_boards
 // @copyright  2014+, Volvo IT
 // ==/UserScript==a
 
@@ -12,18 +13,14 @@
 
     var is_focused = true;
 
-    function freezeHeaders() {
+    //function freezeHeaders() {
 
-        if (!is_focused || $(".content-container.row.content").length < 1) {
-            setTimeout(freezeHeaders, 1000);
-            return;
-        }
+    //    if (!is_focused || $(".content-container.row.content").length < 1) {
+    //        setTimeout(freezeHeaders, 1000);
+    //        return;
+    //    }
 
-        //$('.hub-pivot-content')
-        //	.clone()
-        //	.remove(".content-container.row.content")
-        //	.appendTo($('.hub-pivot-content').parent());
-    }
+    //}
 
     function improveBoard() {
 
@@ -45,7 +42,7 @@
         $.each(allIds, function (itemIndex, itemId) {
             console.log("Kanban improve: querying item: " + itemId);
 
-            $.get("http://tfs2010.it.volvo.net:8080/tfs/Global/_api/_wit/workitems?ids=" + itemId, function (data) {
+            $.get("//" + window.location.host + "/tfs/Global/_api/_wit/workitems?ids=" + itemId, function (data) {
 
                 var itemIsBlocked = data.__wrappedArray[0].fields["10160"] || "No";
                 var itemClassification = data.__wrappedArray[0].fields["10986"] || "";
@@ -54,34 +51,34 @@
 
                 var $itemElm = $(".board-tile[data-item-id=" + itemId + "]");
 
-                if (itemClassification == 'CRExpedited' || itemClassification == 'ATExpedited') {
-                    $itemElm.addClass('expediter');
-                } else {
-                    $itemElm.removeClass('expediter');
-                }
+                setClassBasedOnCondition($itemElm, function () {
+                    itemClassification == 'CRExpedited' || itemClassification == 'ATExpedited' },
+                    'expediter');
 
-                if (itemIsBlocked == "Yes") {
-                    $itemElm.addClass('blocked');
-                } else {
-                    $itemElm.removeClass('blocked');
-                }
+                setClassBasedOnCondition($itemElm, function () {
+                    itemIsBlocked == "Yes" },
+                    'blocked');
 
-                if (itemClassification == 'CR') {
-                    $itemElm.addClass('cr');
-                } else {
-                    $itemElm.removeClass('cr');
-                }
+                setClassBasedOnCondition($itemElm, function () {
+                    itemClassification == 'CR' }, 
+                    'cr');
 
-                if (itemClassification == 'AT') {
-                    $itemElm.addClass('at');
-                } else {
-                    $itemElm.removeClass('at');
-                }
+                setClassBasedOnCondition($itemElm, function () {
+                    itemClassification == 'AT' }, 
+                    'at');
 
             });
         });
 
         setTimeout(improveBoard, 10000);
+    }
+
+    function setClassBasedOnCondition($elm, conditionChecker, className) {
+        if (conditionChecker()) {
+            $elm.addClass(className);
+        } else {
+            $elm.removeClass(className);
+        }
     }
 
     $(function () {
@@ -90,20 +87,11 @@
             improveBoard();
         }
 
-        freezeHeaders();
+        //freezeHeaders();
 
         $(window)
             .focus(function () { is_focused = true; })
             .blur(function () { is_focused = false; });
     });
 
-    function addGlobalStyle(css) {
-        var head, style;
-        head = document.getElementsByTagName('head')[0];
-        if (!head) { return; }
-        style = document.createElement('style');
-        style.type = 'text/css';
-        style.innerHTML = css;
-        head.appendChild(style);
-    }
 })();
